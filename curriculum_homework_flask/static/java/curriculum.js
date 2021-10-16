668754b6893fe36a3783432954e2860ede3f76df
Blosdz <johan_sokar@hotmail.com>
2021-10-16 15:15:47 -0500

    Delete curriculum_homework_flask directory

--- curriculum_homework_flask/static/java/curriculum.js
+++
@@ -1,8 +0,0 @@
-
-
-console.log("hereisformjs")
-let btn=document.querySelector("#btn");
-let sidebar=document.querySelector(".sidebar");
-btn.onclick=function(){
-    sidebar.classList.toggle("active");
-}
